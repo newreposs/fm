0904fffc5fe502b57c990243a562d63ecec32408
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-26 21:53:07 +0300

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -70,7 +70,7 @@ async def addfilter(client, message):
             btn = rm.inline_keyboard
             msg = get_file_id(message.reply_to_message)
             if msg:
-                fileid = msg.id
+                fileid = msg.file_id
                 reply_text = message.reply_to_message.caption.html
             else:
                 reply_text = message.reply_to_message.text.html
@@ -85,7 +85,7 @@ async def addfilter(client, message):
     elif message.reply_to_message and message.reply_to_message.media:
         try:
             msg = get_file_id(message.reply_to_message)
-            fileid = msg.id if msg else None
+            fileid = msg.file_id if msg else None
             reply_text, btn, alert = parser(message.reply_to_message.caption.html, text)
         except Exception:
             reply_text = ""
