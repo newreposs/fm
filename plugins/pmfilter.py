d5f5d90fa0394b5238751ad40727afab782e8cba
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-26 21:50:11 +0300

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -22,8 +22,18 @@ from functions.tools import parser, split_quotes
 
 def get_file_id(msg: Message):
     if msg.media:
-        for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
-            if obj := getattr(msg, message_type):
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
 
