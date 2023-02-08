664ccf9118bf1526c8ba8c57d3b51e997e15dcfc
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-08 11:13:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     APP_ID = os.environ.get("APP_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     OWNER_ID = os.environ.get("OWNER_ID", 'mmagneto') 
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
     KANAL = os.environ.get("KANAL", "")
@@ -20,4 +21,5 @@ class Config:
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     SAVE_USER = "yes" 
-    
+    userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+    userbot.start()
